573cc1f85f7b49cb952b537a43870031a294d400
Joseph <borkowski.joseph@gmail.com>
2023-08-27 13:20:29 -0400

    complete

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
-// Code Keypad Component Here
+import React from "react";  //Every component should have one.
 
 function Keypad (){
+    const handlePasswordChange = (e) => {  //create const.
+        console.log("Entering password..."); //what assignment asked to see.
+    }
+    
+    
+    /*Put a <input /> with an event listener*/
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handlePasswordChange} />  
+        </div>
     )
 }
 
